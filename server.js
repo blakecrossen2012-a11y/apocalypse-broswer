3e664d6e61ceb2dac8c2bca69f462e36c05e3608
blakecrossen2012-a11y <blakecrossen2012@gmail.com>
2026-02-10 20:28:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const PORT = process.env.PORT || 8081;
 const WebSocket = require('ws');
+
 const wss = new WebSocket.Server({ port: PORT });
-
-console.log(`Server running on port ${PORT}`);
-
-let players = {};
+let players = [];
 
 wss.on('connection', ws => {
-  const id = Date.now();
-  players[id] = {id, x:0, y:0, z:0, ws};
-  console.log("Player connected:", id);
+    console.log("New player connected");
 
-  ws.on('message', msg=>{
-    // Broadcast to all
-    for(let pid in players){
-      if(players[pid].ws.readyState === WebSocket.OPEN) players[pid].ws.send(msg.toString());
-    }
-  });
+    ws.on('message', message => {
+        // Broadcast message to all clients
+        players.forEach(p => {
+            if (p !== ws && p.readyState === WebSocket.OPEN) {
+                p.send(message);
+            }
+        });
+    });
 
-  ws.on('close', ()=>{
-    console.log("Player disconnected:", id);
-    delete players[id];
-  });
+    ws.on('close', () => {
+        console.log("Player disconnected");
+        players = players.filter(p => p !== ws);
+    });
 
-  ws.send(JSON.stringify({type:'welcome',id}));
+    players.push(ws);
 });
+
+console.log(`Server running on port ${PORT}`);
