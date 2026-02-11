41f1b087322c4315c5ac480eba37358188eabdd9
blakecrossen2012-a11y <blakecrossen2012@gmail.com>
2026-02-10 21:50:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,18 @@
+const express = require("express");
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
 const WebSocket = require("ws");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 const PORT = process.env.PORT || 8080;
 
-// Serve static files
-const server = http.createServer((req, res) => {
-  let filePath = "." + req.url;
-  if (filePath === "./") filePath = "./index.html";
-
-  const ext = path.extname(filePath);
-  const contentType =
-    ext === ".html" ? "text/html" :
-    ext === ".js" ? "application/javascript" :
-    ext === ".css" ? "text/css" :
-    ext === ".png" ? "image/png" :
-    ext === ".jpg" ? "image/jpeg" :
-    "text/plain";
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      res.writeHead(404);
-      res.end("Not found");
-    } else {
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content);
-    }
-  });
-});
-
-// Attach WebSocket to same server
-const wss = new WebSocket.Server({ server });
+app.use(express.static("public"));
+app.use("/node_modules", express.static("node_modules"));
 
 wss.on("connection", (ws) => {
   console.log("Player connected");
-
-  ws.on("message", (message) => {
-    // Broadcast to all players
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
-  });
-
-  ws.on("close", () => {
-    console.log("Player disconnected");
-  });
 });
 
 server.listen(PORT, () => {
